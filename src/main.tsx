f8414146e90f62428224a63e17908cf7bfe48318
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2025-01-04 07:31:19 +0000

    add: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { Top } from './routes/Top';
+import { About } from './routes/About';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/routes/Top.tsx
@@ -0,0 +1,8 @@
+export const Top = () => {
+  return (
+    <div>
+      <h1>Top</h1>
+      <p>This is the top page.</p>
+    </div>
+  );
+};
