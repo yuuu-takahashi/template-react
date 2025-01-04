8d0a440575d922d6a7347646cecc42bc6075c2a0
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2025-01-04 15:52:46 +0000

    update: upgrade React and React DOM to version 19; switch from react-router-dom to react-router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 import { MainLayout, AuthLayout } from '@/components/layouts';
 import {
