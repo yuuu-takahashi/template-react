16d31af9933e48211a0073a41fc67469f1c0527c
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2025-01-08 15:07:01 +0000

    refactor: split Game component into separate Board and Square components; add utility functions

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,12 +1,12 @@
 import { logo } from '@/assets';
-import { UIImage } from '@/components/ui/UIImage';
+import { Image } from '@/components/ui/Image';
 
 export const Top = () => {
   return (
     <div>
       <h1>Top</h1>
       <p>This is the top page.</p>
-      <UIImage isEager alt="" height={100} src={logo} width={100} />
+      <Image isEager alt="" height={100} src={logo} width={100} />
     </div>
   );
 };
