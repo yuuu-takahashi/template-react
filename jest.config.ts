49b0226780d2991150c6e45551ee2140fdfe734e
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2025-01-11 05:11:26 +0000

    refactor: add moduleNameMapper for improved path resolution in Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
